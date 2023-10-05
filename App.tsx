f1101b944f6556389adcfd17c3f18b9e97fd2c8d
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-05 00:58:51 -0300

    refactor: change login page filename

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from './src/login-page';
+import Login from './src/LoginPage';
 
 export default function App() {
   return (
