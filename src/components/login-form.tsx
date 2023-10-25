ae0489e64107e4b430c7ea43cc737ece9554a17e
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-25 19:28:25 -0300

    feat: create handle login function & add onpress in login button

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,5 +1,5 @@
 import {
-  View, Text, TextInput, Button,
+  View, Text, TextInput, Button, Alert,
 } from 'react-native';
 import React, { useState } from 'react';
 
@@ -7,10 +7,9 @@ export default function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    console.log('Email:', email);
-    console.log('Password:', password);
-  };
+  const handleLogin = () => (email && password
+    ? Alert.alert('Logado com sucesso!', `Informações de login: \nEmail: ${email} \nSenha: ${password}`)
+    : Alert.alert('Preencha todos os campos!'));
 
   return (
     <View>
