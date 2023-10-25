f5405f96066969816ac9c5685a4f598d44e5c471
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-10-25 19:07:23 -0300

    refactor: changes components naming to kebab case

--- src/login-page.tsx
+++ src/login-page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Header from './components/Header';
+import Header from './components/header';
 
 export default function Login() {
   return (
