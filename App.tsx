7f932b39c4a99493e7885b1a7d7b623df23aec7a
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-18 20:44:12 -0300

    refactor: rename login page & pages folder

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from './src/pages/login-page';
+import Login from './src/pages/login';
 import Home from './src/pages/home';
 
 const Stack = createNativeStackNavigator();
