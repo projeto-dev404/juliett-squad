d622efbc517f600e4b49e6f077c852fdc52b332e
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-18 12:04:53 -0300

    refactor: use app styles in same file

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import styled from 'styled-components/native';
 import Login from './src/pages/login-page';
-import Container from './AppStyle';
+
+const Container = styled.View`
+    flex: 1;
+    background-color: #fff;
+    align-items: center;
+    justify-content: center;
+`;
 
 export default function App() {
   return (
