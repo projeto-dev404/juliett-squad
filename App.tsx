17c2ae51baaae173bdcafcf2a0b46fc66d95e24f
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-18 20:45:00 -0300

    refactor: app screens imports

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from './src/pages/login';
-import Home from './src/pages/home';
+import Login from './src/screens/login';
+import Home from './src/screens/home';
 
 const Stack = createNativeStackNavigator();
 
