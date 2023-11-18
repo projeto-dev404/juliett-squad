c0d8659e0614fe7dddf7082bf4812606b05eedcf
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-18 20:39:33 -0300

    feat: create home page & add to navigation

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Login from './src/pages/login-page';
+import Home from './src/pages/home';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/pages/home/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+
+const HomeText = styled.Text`
+  font-size: 40px;
+  text-align: center;
+`;
+
+export default HomeText;
