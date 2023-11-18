1b76dbd78d353c37ac28bb3666f98baa05ea8a7b
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2023-11-18 12:03:14 -0300

    fix: logo import path

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import codeMentorLogo from '../assets/code-mentor-logo.jpeg';
+import codeMentorLogo from '../../assets/code-mentor-logo.jpeg';
 import LogoImage from './style';
 
 export default function Header() {
